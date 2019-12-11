0d0d5d1881bce83158dcd758ff1feb1ef808b551
owl10124 <shevonnechia1@gmail.com>
2019-12-11 21:53:26 +0800

    android

---
+++ source/Android/app/src/main/java/tk/owl10124/crisismanual/SplashActivity.java
@@ -0,0 +1,24 @@
+package tk.owl10124.crisismanual;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        findViewById(R.id.startBtn).setOnClickListener(e->launch());
+    }
+
+    public void launch() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+}
